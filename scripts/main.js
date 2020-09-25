1079e5d67f924a3566323156656ec48a20a65bcc
Jenni <justlittleoldmejenn@gmail.com>
2020-09-25 14:12:47 -0700

    Error after Chapter 11
    $label is not defined checklist.js
    cannot read property 'Formhandler' main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,22 @@
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
-    var FormHandler = App.FormHandler;
-    var myTruck = new Truck('KITT', new DataStore());
+
     window.myTruck = myTruck;
-
+    var CheckList = App.CheckList;
+    var myTruck = new Truck('KITT', new DataStore());
     var formHandler = new FormHandler(FORM_SELECTOR);
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    // var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    var CheckList = App.CheckList;
 
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.creatOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
+
+
     console.log(formHandler);
-})(window);
+})(window)
